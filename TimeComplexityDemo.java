2973eb01897119f06c27c03a4e4666ec3a403480
tejuspe <107438344+tejuspe@users.noreply.github.com>
2022-08-15 22:03:28 +0530

    New
    Something New here

---
+++ TimeComplexityDemo.java
@@ -0,0 +1,29 @@
+public class TimeComplexityDemo 
+{
+    public static void main(String[] args)
+    {
+        double now = System.currentTimeMillis();
+
+        TimeComplexityDemo demo = new TimeComplexityDemo();
+        System.out.println(demo.findSum(77777));
+
+        System.out.println("Time taken- " +(System.currentTimeMillis() - now) + " millisecs. ");
+    }
+
+    public int findSum(int n)
+    {
+        return n * (n+1) /2;
+    }
+    
+
+    /*public int findSum(int n)
+    {
+        int sum= 0;
+        for(int i=0;i<=n;i++)
+        {
+            sum = sum +i;
+        }
+        return sum;
+    } */
+    
+}
